1d4be4c3e8fceecaa092c51b9f0f8ac38e272829
Daniel Burgoa <danielburgoa50@gmail.com>
2024-03-08 19:50:05 -0500

    white screen fixed

---
+++ script.js
@@ -0,0 +1,12 @@
+"use strict";
+
+document.addEventListener("DOMContentLoaded", function () {
+  var backgroundImage = new Image();
+  backgroundImage.src = "/media/main-background.webp";
+
+  backgroundImage.onload = function () {
+    // Background image has loaded
+    document.querySelector("body").classList.remove("hidden");
+    document.querySelector(".loader").classList.add("hidden");
+  };
+});
