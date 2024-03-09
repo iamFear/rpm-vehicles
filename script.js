7584f48721465eefa70b549f41acec16133ce827
Daniel Burgoa <danielburgoa50@gmail.com>
2024-03-09 11:24:03 -0500

    navbar finished

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 "use strict";
 
+// Prevents the website content loading before the background image
 document.addEventListener("DOMContentLoaded", function () {
   var backgroundImage = new Image();
   backgroundImage.src = "/media/main-background.webp";
